33bfbf847f233fac412fe778b84af349c2927b87
lizzb <huzzah.its.lizz@gmail.com>
2013-12-11 12:54:36 -0600

    moved over more shiz

---
+++ js/itemPriceList.js
@@ -0,0 +1,684 @@
+var products = [
+	{
+		name: "Celery + Carrots (Crudites)",
+		price: "2.69",
+		// image: "",
+		// id
+		// options/variations
+		section: "Deli",
+	},
+	{
+		name: "Edamame",
+		price: "2.95",
+		section: "Deli",
+	},
+	{
+		name: "Chicken Caesar Salad",
+		price: "5.59",
+		section: "Deli",
+	},
+	{
+		name: "Tomato + Mozzerella Baguette",
+		price: "4.99",
+		section: "Deli",
+	},
+	{
+		name: "Sushi - California",
+		price: "0.00", //......
+		section: "Deli",
+	},
+	{
+		name: "Sushi - California w/ Masago",
+		price: "0.00", //......
+		section: "Deli",
+	},
+	{
+		name: "Sushi - Shrimp",
+		price: "0.00", //......
+		section: "Deli",
+	},
+	{
+		name: "Sushi - Veggie",
+		price: "0.00", //......
+		section: "Deli",
+	},
+	{
+		name: "PB&J on Wheat",
+		price: "2.99",
+		section: "Deli",
+	},
+	// ---------- Candy, Chips, & Soda ---------- //
+	{
+		name: "Nerd Rope",
+		price: "1.19",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Tostido's Lime Tortilla Chips",
+		price: "4.29",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Spicy Cheeto Fries",
+		price: "1.49",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Sweettart Gummies",
+		price: "2.75",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Chocolate Candy Bar",
+		price: "0.00", //......
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Dorito's",
+		price: "0.00", //......
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Candy Bar",
+		price: "1.19",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "M&Ms",
+		price: "1.19",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "M&Ms",
+		price: "1.19",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Soda - 12 pack cans",
+		price: "0.00", //......
+		section: "Candy, Chips, & Soda",
+	},
+
+	// ---------- Baked Goods ---------- //
+	{
+		name: "Cookie",
+		price: "0.00", //......
+		section: "Baked Goods",
+	},
+	{
+		name: "Danish",
+		price: "2.25",
+		section: "Baked Goods",
+	},
+	{
+		name: "Brownie",
+		price: "1.99",
+		section: "Baked Goods",
+	},
+
+
+	// --------- Packaged Noodles/Soup ---------- //
+	{
+		name: "Simply Asia Soup/Noodles",
+		price: "4.99",
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Cup O Ramen",
+		price: "0.00", //......
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Top Ramen Package",
+		price: "0.49",
+		section: "Packaged Noodles/Soup",
+	},
+
+	// --------- Freezer ---------- //
+	{
+		name: "Lean Cuisine",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Amy's Pesto Tortellini Bowl",
+		price: "5.49",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Taipei Stir Fry Chinese Takeout Boxes",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Bagel Bites Mini Pizzas",
+		price: "0.00", //......
+		section: "Frozen Meals",
+	},
+	{
+		name: "Naanwich",
+		price: "0.00", //......
+		section: "Frozen Meals",
+	},
+
+
+	{
+		name: "Fruit Water",
+		price: "1.99", // 16 oz
+		section: "Cold Drinks",
+	},
+	{
+		name: "Bottled Soft Drinks",
+		price: "1.69", // 20 oz 
+		section: "Cold Drinks",
+	},
+
+
+	{
+		name: "Fridge Packs (12 cans)",
+		price: "6.99",
+		section: "Candy, Chips, & Soda",
+	},
+
+
+
+
+	{
+		name: "DiGorno Pizza",
+		price: "5.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "EVOL Bowls",
+		price: "5.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Amy Entree Bowls",
+		price: "5.49",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Spaghetti with Meat Sauce",
+		price: "5.29",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Lean Cuisine Glazed Chicken",
+		price: "5.29",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Lean Cuisine Lasagna ",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Lean Cuisine Ravioli",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Lean Cuisine Fettuccini Alfredo",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "TGIF Buffalo Wings",
+		price: "4.49",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Hot Pockets",
+		price: "1.29",
+		section: "Frozen Meals",
+	},
+
+	{
+		name: "Ben & Jerry's Ice Cream Pints ",
+		price: "4.99",
+		section: "Frozen Treats",
+	},
+{
+		name: "Starbucks Ice Cream Pints ",
+		price: "4.99",
+		section: "Frozen Treats",
+	},
+{
+		name: "Haagen Daz Ice Cream Pints ",
+		price: "4.99",
+		section: "Frozen Treats",
+	},
+{
+		name: "Dove Ice Cream Bars",
+		price: "2.99",
+		section: "Frozen Treats",
+	},
+{
+		name: "Ben & Jerry's Ice Cream Bars",
+		price: "2.69",
+		section: "Frozen Treats",
+	},
+{
+		name: "Ice Cream Cookie Toll House",
+		price: "2.59",
+		section: "Frozen Treats",
+	},
+{
+		name: "Mrs. Fields Ice Cream Bar",
+		price: "2.50",
+		section: "Frozen Treats",
+	},
+{
+		name: "Twix Ice Cream Bar",
+		price: "2.29",
+		section: "Frozen Treats",
+	},
+ 
+{
+		name: "Snickers Ice Cream Bar",
+		price: "2.29",
+		section: "Frozen Treats",
+	},
+{
+		name: "Nestle Drumstick Vanilla",
+		price: "2.25",
+		section: "Frozen Treats",
+	},
+
+
+	// ---------- Cold Drinks ---------- //
+	{
+		name: "Odwalla Juice Smoothie",
+		price: "3.49",
+		section: "Cold Drinks",
+	},
+	{
+		name: "Soda - Individual bottle",
+		price: "0.00", //......
+		section: "Cold Drinks",
+	},
+	{
+		name: "Wheat Thins",
+		price: "0.00", //......
+		section: "",
+	},
+	{
+		name: "Box of cereal",
+		price: "0.00", //......
+		section: "",
+	},
+	{
+		name: "Mini bowl of cereal",
+		price: "0.00", //......
+		section: "",
+	},
+	{
+		name: "Clif Bar",
+		price: "0.00", //......
+		section: "",
+	},
+
+	// ---------- Register ---------- //
+	{
+		name: "Laffy Taffy",
+		price: "0.25",
+		section: "Register",
+	},
+	{
+		name: "Mini Reese's Cups",
+		price: "0.25",
+		section: "Register",
+	},
+	{
+		name: "Gum",
+		price: "0.00", //......
+		section: "Register",
+	},
+	{
+		name: "Machine Coffee Drink 16 oz",
+		price: "3.00",
+		section: "Register",
+	},
+
+	{
+		name: "Soup of the day",
+		price: "3.49",
+		section: "Register",
+	},
+	/*{
+		name: "",
+		price: "",
+		section: "",
+	},
+	*/
+
+	{
+		name: "Smoothies",
+		price: "3.50",
+		section: "Register",
+	},
+	{
+		name: "Coffee - Small",
+		price: "1.65",
+		section: "Register",
+	},
+	{
+		name: "Coffee - Medium",
+		price: "1.90",
+		section: "Register",
+	},
+	{
+		name: "Coffee - Large",
+		price: "2.00",
+		section: "Register",
+	},
+
+
+
+
+
+
+	{
+		name: "Campbell's Soup at Hand",
+		price: "2.39",
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Hand Snacks Pudding",
+		price: "2.99 ", //3.5 oz 
+		section: "",
+	},
+
+
+	{
+		name: "Sue Bee Honey",
+		price: "6.59", // 12 oz 
+		section: "",
+	},
+	{
+		name: "Smucker's Jelly ",
+		price: "3.99", // 12 oz 
+		section: "",
+	},
+	{
+		name: "Peter Pan",
+		price: "4.99", // 16.3 oz 
+		section: "",
+	},
+	{
+		name: "Nutella Spread",
+		price: "5.99", // 13 oz 
+		section: "",
+	},
+	{
+		name: "Loaf of Bread",
+		price: "1.99", // 4 oz 
+		section: "",
+	},
+	{
+		name: "Quaker Medleys (Oatmeal)",
+		price: "2.79",
+		section: "",
+	},
+
+
+	{
+		name: "Pop Tarts 2pk",
+		price: "1.29",
+		section: "",
+	},
+	{
+		name: "Balance Bars",
+		price: "1.99",
+		section: "",
+	},
+	{
+		name: "KIND Bars",
+		price: "2.29",
+		section: "",
+	},
+
+
+	{
+		name: "Sabra Hummus Cups",
+		price: "3.79", // 4.56 oz
+		section: "",
+	},
+	{
+		name: "Sabra Salsa cups ",
+		price: "3.79", // 4.1 oz
+		section: "",
+	},
+	{
+		name: "Odwalla Drinks",
+		price: "3.29", // 12 oz 
+		section: "",
+	},
+	{
+		name: "Zaco Water",
+		price: "3.19", // 14 oz 
+		section: "",
+	},
+	{
+		name: "Cheese Sticks",
+		price: "1.09", // 1 oz
+		section: "",
+	},
+	{
+		name: "Silk Soy Singles",
+		price: "1.79", // 8 oz
+		section: "",
+	},
+	{
+		name: "Silk Soy Quarts",
+		price: "2.99", // 32 oz
+		section: "",
+	},
+	{
+		name: "Oikos Greek Yogurt",
+		price: "1.99", // 5.3 oz
+		section: "",
+	},
+	{
+		name: "PB Jamwich",
+		price: "1.59", // 2.8 oz
+		section: "",
+	},
+	
+
+	{
+		name: "Snyder's Snacks ",
+		price: "1.29",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Gardetto's Snacks",
+		price: "2.99", // 5.5 oz 
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Rye Street Chips",
+		price: "1.29", // 1.5 oz 
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Kettle Chips",
+		price: "1.29", // 1.5oz 
+		section: "Candy, Chips, & Soda",
+	},
+
+
+	{
+		name: "Keebler Sugar Wafers ",
+		price: "1.19", // 2.75 oz
+		section: "",
+	},
+	
+];
+
+var lizzproducts = [
+{
+		name: "Odwalla Drinks",
+		price: "3.29", // 12 oz 
+		section: "",
+	},
+	{
+		name: "Soup of the day",
+		price: "3.49",
+		section: "Register",
+	},
+	{
+		name: "Campbell's Soup at Hand",
+		price: "2.39",
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Bottled Soft Drinks",
+		price: "1.69", // 20 oz 
+		section: "Cold Drinks",
+	},
+	{
+		name: "Fridge Packs (12 cans)",
+		price: "6.99",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Celery + Carrots (Crudites)",
+		price: "2.69",
+		// image: "",
+		// id
+		// options/variations
+		section: "Deli",
+	},
+	{
+		name: "Edamame",
+		price: "2.95",
+		section: "Deli",
+	},
+	{
+		name: "Chicken Caesar Salad",
+		price: "5.59",
+		section: "Deli",
+	},
+	{
+		name: "Tomato + Mozzerella Baguette",
+		price: "4.99",
+		section: "Deli",
+	},
+	{
+		name: "Nerd Rope",
+		price: "1.19",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Tostido's Lime Tortilla Chips",
+		price: "4.29",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Spicy Cheeto Fries",
+		price: "1.49",
+		section: "Candy, Chips, & Soda",
+	},
+	{
+		name: "Sweettart Gummies",
+		price: "2.75",
+		section: "Candy, Chips, & Soda",
+	},
+		{
+		name: "Simply Asia Soup/Noodles",
+		price: "4.99",
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Top Ramen Package",
+		price: "0.49",
+		section: "Packaged Noodles/Soup",
+	},
+	{
+		name: "Lean Cuisine",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Lean Cuisine Glazed Chicken",
+		price: "5.29",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Amy's Pesto Tortellini Bowl",
+		price: "5.49",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Taipei Stir Fry Chinese Takeout Boxes",
+		price: "4.99",
+		section: "Frozen Meals",
+	},
+	{
+		name: "Bagel Bites Mini Pizzas",
+		price: "0.00", //......
+		section: "Frozen Meals",
+	},
+	{
+		name: "Naanwich",
+		price: "0.00", //......
+		section: "Frozen Meals",
+	},
+
+];
+
+/*
+NutriGrain Bar
+11.5 oz 3.79
+Diamon Cutlery 24ct 1.99
+*/
+
+	// section: "Frozen Meals",
+	// section: "Baked Goods",
+	// section: "Candy, Chips, & Soda",
+	// section: "Register",
+	// section: "Deli",
+	// section: "Packaged Noodles/Soup",
+	// section: "Cold Drinks",
+	// section: "Frozen Treats",
+
+
+function makeProductList() {
+
+	var productlisthtml = "";
+
+	$.each( products, function( index ) {
+	  	productlisthtml +=
+	  	"<li class='product purchase btn btn-info item' id='"+ index+ "'>" +
+	  	"<span class='title'>" + products[index].name + "</span>" +
+	  	"<span class='price'> $" + products[index].price  + "</span> </li>";
+	});
+
+	document.getElementById("products").innerHTML = "<ul>" + productlisthtml + "</ul>";
+}
+
+function sortAZ() {
+	var productList = $('#products ul');
+	var listItems = productList.children('li').get();
+	listItems.sort(function(a, b) {
+		var compA = $(a).text().toUpperCase();
+		var compB = $(b).text().toUpperCase();
+   return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+})
+$.each(listItems, function(idx, itm) { productList.append(itm); });
+}
+
+function sortByPrice() {
+  var productList = $('#products ul');
+  var listItems = productList.children('li').get();
+  listItems.sort(function(a,b){
+    var compA = $(a).find('.price').text();
+    var compB = $(b).find('.price').text();
+    //return (compA < compB) ? -1 : (compA > compB) ? 1 : 0; original, ascending
+    return (compA > compB) ? -1 : (compA < compB) ? 1 : 0;
+  });
+  $(productList).append(listItems);
+}
+
---
+++ js/mealHoursEQ.js
@@ -0,0 +1,310 @@
+	var breakfastEQ = 5.00;
+	var brunchEQ = 9.00;
+	var lunchEQ = 7.00;
+	var dinnerEQ = 9.00;
+	var latenightEQ = 7.00;
+	
+
+	var breakfastStartH = 7;	// 7:30 am
+	var breakfastStartM = 30;
+
+	var breakfastEndH = 10;		// 10:45 am
+	var breakfastEndM = 45;
+	
+	var lunchStartH = 10;		// 10:46 am
+	var lunchStartM = 46;
+
+	var lunchEndH = 16;			// 4:45 am
+	var lunchEndM = 45;
+
+	var dinnerStartH = 16;		// 4:46 pm
+	var dinnerStart = 46;
+	
+	var dinnerEndH = 19; 		// 7:30pm
+	var dinnerEndM = 30;
+
+
+
+var total = 0.00;
+var eq = 0.00;
+var mealCount = 0;
+/*
+var products = [
+	{
+		name: "Celery + Carrots (Crudites)",
+		price: "2.69",
+		section: "Deli",
+	},
+	{
+		name: "Edamame",
+		price: "2.95",
+		section: "Deli",
+	},
+	{
+		name: "Chicken Caesar Salad",
+		price: "5.59",
+		section: "Deli",
+	},
+];
+
+var cart = [
+	{
+		name: "Celery + Carrots (Crudites)",
+		price: "2.69",
+		quantity: 1,
+		item-id: 1,
+	},
+];
+*/
+var dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", 
+                                "Thursday", "Friday", "Saturday"];
+
+
+//window.addEventListener('load', function(e) {
+//  document.querySelector('#test').innerHTML = 'Hello, Jsany!';
+
+//}, false);
+
+//
+//
+//
+function start()
+{
+	updateEQ();
+	/////updateTotal();
+
+	getCurrentDayTimeString();
+
+}
+
+
+//
+//
+//
+// Readable for the user
+function getCurrentDayTimeString()
+{
+	var today = new Date();
+	var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+
+    var hour =	((h < 10)? "0" : "") + ( (h > 12)? (h-12) : h);
+    var min =	((m < 10)? "0" : "") + m;
+    var sec =	((s < 10) ? "0" : "") + s;
+    var ampm = 	((h > 12) ? " PM" : " AM");
+
+    document.getElementById('currentWeekday').innerHTML = dayNames[today.getDay()];
+        
+    document.getElementById('currentTime').innerHTML = hour + ":" + min + ":" + sec + ampm;
+
+ 	document.getElementById('eq').innerHTML = toUSD(eq);
+
+    // t=setTimeout(function(){startTime()},500);
+    //<body onload="startTime();">
+}
+
+//
+//
+//
+function updateEQ()
+{
+	var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+
+
+/*if 	( 	(h > lunchStartH && h < lunchEndH) &&
+		(m > lunchStartM && m < lunchEndM) )
+{
+	eq = lunchEQ;
+}*/
+
+//else if
+if 	(	(h > dinnerStartH && h < dinnerEndH) && 
+		(m > dinnerStartM && m < dinnerEndM) )
+{
+	eq = dinnerEQ;
+}
+else eq = latenightEQ;
+
+//check if dinnertime
+//check if breakfast
+// check if sunday "brunch"
+// if none, eq = lunch/latente value
+
+//alert('h: '+h+'m: '+m+'eq: '+eq);
+}
+/*
+Mon - Thur 	11:00 a.m. - 2:00 a.m.
+Friday 		11:00 a.m. - 2:00 a.m.
+Saturday
+Sunday
+
+11 fri - 2 sat
+11 sat - 2 sun
+12 sunday - 2 mon
+*/
+function fun(){
+
+
+}
+
+
+function incrementItem(){
+
+
+}
+
+function decrementItem(){
+
+	// if quantity <=0 remove frmo cart
+}
+
+function emptyCart(){
+
+
+}
+
+
+function filter(){
+
+
+}
+
+/*
+function getCurrentDayTimeString()
+{
+
+	// getCurrentWeekdayString()
+	document.getElementById('currentWeekday').innerHTML=dayNames[today.getDay()];
+	//return hour + ":" + min + ":" + sec + ampm;
+	//document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+	document.getElementById('currentTime').innerHTML = hour + ":" + min + ":" + sec + ampm;
+	// t=setTimeout(function(){startTime()},500);
+	//<body onload="startTime();">
+}
+*/
+// http://www.cssnewbie.com/javascript-currency-conversion-script/#.UoQZr5TwIw9
+// Converts a float number to a USD currency format
+// fx isn't perfect, testing "010" == $8.00??
+function toUSD(number) {
+    var number = number.toString(), 
+    dollars = number.split('.')[0], 
+    cents = (number.split('.')[1] || '') +'00';
+
+    // converts our string into an array of individual digits
+    dollars = dollars.split('')
+    	// reverses our array
+    	.reverse()
+    	// turns our array back into a single string again
+    	.join('')
+    	// “add a comma to the end of every group of 3 numbers, 
+    	// unless it is the last group of three numbers
+    	// (to avoid trailing commas)”:
+        .replace(/(\d{3}(?!$))/g, '$1,')
+        // converts our newly comma’d string to an array again:
+        .split('')
+        // puts the numbers back in the proper order
+        .reverse()
+        // makes them a string again
+        .join('');
+        // return our manipulated number
+        // with a dollar sign out front
+        // and a decimal point between dollars and cents
+        // (slice to only return the first two digits
+        // in the cents string).
+    return '$' + dollars + '.' + cents.slice(0, 2);
+}
+/*
+function formatPrice(price)
+{
+Number.prototype.toCurrencyString = function() { return "$" + Math.floor(this).toLocaleString() + (this % 1).toFixed(2).toLocaleString().replace(/^0/,''); }
+}*/
+
+
+//
+//
+//
+function formatPrice(price)
+{
+	// Formatting variables.
+	var currency = '$00,000.00', //this.options.currencyFormat,
+		before = currency.replace(/^([^\d]+)?.+?$/, '$1'),
+		after = currency.replace(/^.+?([^\d]+)?$/, '$1'),
+		number = currency.substring(before.length, currency.length - after.length),
+		decimal = number.replace(/^.+?([^\d]+)\d{2}$/, '$1');
+
+	decimal = decimal === number ? '' : decimal;
+	number = decimal ? number.substring(0, number.lastIndexOf(decimal)) : number;
+	number = number.split('').reverse();
+	
+	// Split the price to whole & fraction amounts.
+	price = parseFloat(price || 0).toFixed(2).toString().match(/^(.+?)(?:[^\d](\d\d))?$/);
+	price[1] = price[1].replace(/[^\d]/g, '');
+	price[2] = price[2] || 0;
+			
+	// Reformat the price array to contain only whole & fraction
+	// numbers, and reverse the format.
+	price = parseFloat(price[1] + '.' + price[2]).toFixed(2).split('.');
+	price[0] = price[0].split('').reverse();
+	
+	// Build the currency string.
+	currency = [];
+	$.each(number, function (i, number) {
+		currency.push(/^\d+$/.test(number) ? price[0].shift() : number);
+	});
+
+	currency = currency.reverse().join('').replace(/^(?:[^\d]+)?(.+)(?:[^\d]+)?$/, '$1');
+	
+	// Add formatting.
+	price = before + currency + (decimal ? decimal + price[1] : '') + after;
+	return price;
+}
+
+//
+//
+//
+function updateMealPointTotal()
+{
+	var pointOverflow = total % eq;
+	document.getElementById('points-over').innerHTML = pointOverflow;
+	document.getElementById('eq-meals-over').innerHTML = mealCount;
+	document.getElementById('points-under').innerHTML = eq - pointOverflow;
+	document.getElementById('eq-meals-under').innerHTML = mealCount + 1;
+
+	document.getElementById('cart-subtotal').innerHTML = toUSD(total);
+}
+
+function updateSubtotal()
+{
+	// sum up every item in the cart
+
+	// for each item in the cart
+	// multiply price x quantity
+
+}
+
+// remove item from the cart array
+// and also remove the cart item html /redo? the cart
+function removeCartItem()
+{
+
+}
+
+// add item to the array
+// and also generate and append the cart item html
+function addCartItem()
+{
+	updateMealPointTotal();
+}
+
+//update, genrate, create?
+function generateCartHTML()
+{
+	
+}
+
+
+
+
